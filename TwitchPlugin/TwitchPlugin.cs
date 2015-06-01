b87277976964d03d9a3802d5c69e1ea06814c89e
epix37 <epikz37@gmail.com>
2015-06-01 18:49:13 +0200

    v0.3

--- TwitchPlugin/TwitchPlugin.cs
+++ TwitchPlugin/TwitchPlugin.cs
@@ -67,7 +67,7 @@ namespace TwitchPlugin
 
 		public Version Version
 		{
-			get { return new Version(0, 2, 0, 0); }
+			get { return new Version(0, 3, 0, 0); }
 		}
 
 		public MenuItem MenuItem { get; private set; }
